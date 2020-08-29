a5b307af963df7e7a79c1800fa026b003287c89b
Pierre Ziegler <vl4dimyr@gmail.com>
2020-08-29 13:30:55 +0200

    Added: Tooltips

---
+++ ItemStatsMod.cs
@@ -0,0 +1,30 @@
+﻿using ItemStats;
+using RoR2;
+using System.Runtime.CompilerServices;
+
+namespace CommandItemCount
+{
+    static class ItemStatsMod
+    {
+        private static bool? _enabled;
+
+        internal static bool enabled
+        {
+            get
+            {
+                if (_enabled == null)
+                {
+                    _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("dev.ontrigger.itemstats");
+                }
+
+                return (bool)_enabled;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        internal static string GetStats(ItemIndex itemIndex, int itemCount)
+        {
+            return ItemStatProvider.ProvideStatsForItem(itemIndex, itemCount);
+        }
+    }
+}
